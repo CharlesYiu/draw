a0320f2e7dcbf6f6143f1d4ed7e3c0375481393e
Charles Yiu <charlesyiu.w@gmail.com>
2022-04-29 12:54:54 +0800

    panel opacity not working fix

--- script.js
+++ script.js
@@ -6,53 +6,68 @@
 // TODO Implement color bucket
 // TODO use a dictionary instead of an array in 'let pointers...' (Optional)
 // TODO Smooth pencil tool's lines (Not important)
-let pressedShift = false
-let transparentSettings = false
+// TODO cursor support for mobile
+// TODO implement option to change cursor size so people can see where their drawing in touchscreen/mobile
+//  
 
-window.onkeydown = function() {
-    const key = window.event.key
-    if (key === "Shift") {
-        pressedShift = true
-    } else if (key === "Escape") {
-        settings.style.opacity = settings.style.opacity === "0.25" ? "1" : "0.25"
-        settings.style.pointerEvents = settings.style.opacity === "0.25" ? "none" : "auto"
-        transparentSettings = settings.style.opacity === "0.25"
-    } else if (key === "l") {
-        Tools.selected = Tools.LineTool
-        updateTool()
-    } else if (key === "p") {
-        Tools.selected = Tools.PencilTool
-        updateTool()
-    } else if (key === "r") {
-        Tools.selected = Tools.SquareTool
-        updateTool()
-    } else if (key === "c") {
-        Tools.selected = Tools.CircleTool
-        updateTool()
-    } else if (key === "e") {
-        Tools.selected = Tools.EraseTool
-        updateTool()
-    } else if (key === "=") {
-        thicknessInput.valueAsNumber += 1
-    } else if (key === "-") {
-        thicknessInput.valueAsNumber -= 1
+let pressedShift = false
+let transparentPanel = false
+
+// shortcuts
+window.onkeydown = function(event) {
+    switch (event.key) {
+        default:
+            break
+        case "Shift":
+            pressedShift = true
+            break
+        case "Tab":
+            panel.style.opacity = panel.style.opacity === "0.25" ? "1" : "0.25"
+            panel.style.pointerEvents = panel.style.opacity === "0.25" ? "none" : "auto"
+            transparentPanel = panel.style.opacity === "0.25"
+            break
+        case "l":
+            Tools.selected = Tools.LineTool
+            updateTool()
+            break
+        case "p":
+            Tools.selected = Tools.PencilTool
+            updateTool()
+            break
+        case "r":
+            Tools.selected = Tools.SquareTool
+            updateTool()
+            break
+        case "c":
+            Tools.selected = Tools.CircleTool
+            updateTool()
+            break
+        case "e":
+            Tools.selected = Tools.EraseTool
+            updateTool()
+            break
+        case "=":
+            thicknessInput.valueAsNumber += 1
+            break
+        case "-":
+            thicknessInput.valueAsNumber -= 1
+            break
     }
 }
+
 window.onkeyup = function() {
     const key = window.event.key
-    if (key === "Shift") {
-        pressedShift = false
-    }
+    if (key === "Shift") pressedShift = false
 }
 
 window.addEventListener("load", Tools.Handle)
 canvas.addEventListener("mousedown", function() {
-    settings.style.opacity = "0.25"
-    settings.style.pointerEvents = "none"
+    panel.style.opacity = "0.25"
+    panel.style.pointerEvents = "none"
 })
 canvas.addEventListener("mouseup", function() {
-    if (!transparentSettings) {
-        settings.style.opacity = "1"
-        settings.style.pointerEvents = "auto"
+    if (!transparentPanel) {
+        panel.style.opacity = "1"
+        panel.style.pointerEvents = "auto"
     }
 })
